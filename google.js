b4b4b6c6b94b2c5cfa257023a2a90e9959c423e8
ashwin97 <ashwix1997@gmail.com>
2021-01-15 19:33:09 -0800

    list all files in dir

--- google.js
+++ google.js
@@ -25,21 +25,21 @@ async function main(){
     // The transcribed audio
     var transcription;
     
-//     //requiring path and fs modules
-// //joining path of directory 
-// const directoryPath = path.join(__dirname, 'Documents');
-// //passsing directoryPath and callback function
-// fs.readdir(directoryPath, function (err, files) {
-//     //handling error
-//     if (err) {
-//         return console.log('Unable to scan directory: ' + err);
-//     } 
-//     //listing all files using forEach
-//     files.forEach(function (file) {
-//         // Do whatever you want to do with the file
-//         console.log(file); 
-//     });
-// });
+    //requiring path and fs modules
+//joining path of directory 
+const directoryPath = path.join(__dirname, 'Documents');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
     
     // (async () => {
     
