ffd474cdc932291bf8bd00d6c3139c70fbda4ea3
ashwin97 <ashwix1997@gmail.com>
2021-01-15 23:33:55 -0800

    configure for google and microsoft

--- public/js/recorder.js
+++ public/js/recorder.js
@@ -1,82 +1,74 @@
 
-var recorder; // globally accessible
+/* GLOBAL VARS */
+var recorder; 
 var mic;
-
-var start_btn = document.getElementById("btn-start-recording");
-var end_btn = document.getElementById("btn-stop-recording");
-
-var file = document.getElementById("file");
-
 var file_blob = new Blob();
 
-start_btn.onclick = function(){
 
-
-    navigator.mediaDevices.getUserMedia({
-        audio: true
-    }).then(async function(stream) {
-        
-        mic = stream;
-        
-        recorder = RecordRTC(stream, {
-            type: 'audio'
-        });
-        recorder.startRecording();
-
+document.querySelectorAll('.btn-start-recording').forEach(item => {
     
-});
-};
-
-
-end_btn.onclick = function(){
-    
-    // Initiates stop recording and saves file 
-    recorder.stopRecording(function() {
-        file_blob = recorder.getBlob();
+    item.addEventListener('click', event => {
         
-        // Convert blob to a file
-        file_blob = blobToFile(file_blob, "audio.wav");
-
-        console.log(file_blob);
-
-        // invokeSaveAsDialog(blob,"audio.wav");
-        
-        // Goes to the route but does not send file as expected
-        // fetch("http://localhost:1234/", { 
-        //     method: 'POST', 
-        //     body:{
-        //         "audio":file_blob
-        //         } 
-        //     })
-        //     .then(results => console.log(results.json))
-        
-        let fd = new FormData();
-        fd.append("audio",file_blob);
-
-        
-        
-        // This releases the mic
-        mic.stop();
-
-        axios({
-            url:"/",
-            method:"POST",
-            data: fd
-        }).then(
-            res => {
-                
-                console.log(res);
-                sleep(2000);
-                window.location.href = "/";
+        // Capture mic
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then(async function(stream) {
+            
+            mic = stream;
+            
+            // Instantiate recorder
+            recorder = RecordRTC(stream, {
+                type: 'audio'
             });
+            
+            // Start recording
+            recorder.startRecording();
+        });
+    })
 
+})
 
+document.querySelectorAll('.btn-stop-recording').forEach(item => {
+
+    item.addEventListener('click', event => {
         
 
-    });
+        console.log("STOPPING RECORDING");
+        // Initiates stop recording and saves file 
+        recorder.stopRecording(function() {
+            file_blob = recorder.getBlob();
+            
+            // Convert blob to a file
+            file_blob = blobToFile(file_blob, "audio.wav");
+
+            console.log(file_blob);
+            
+            // Creaets form data to be sent to server
+            let fd = new FormData();
+            fd.append("audio",file_blob);
+            
+            fd.append("engine",window.location.pathname);
+            // This releases the mic
+            mic.stop();
+
+            axios({
+                url:"/",
+                method:"POST",
+                data: fd
+            }).then(
+                res => {
+                    
+                    console.log(res);
+                    sleep(2000);
+                    window.location.href = window.location.href;
+                });   
+
+        });
 
 
-}
+    })
+})
+
 
 function blobToFile(theBlob, fileName){
 /* 
