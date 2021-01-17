788a33c7c3f815d7fb001c2059cc9735fbfa2814
ashwin97 <ashwix1997@gmail.com>
2021-01-17 00:01:08 -0800

    Add microsoft as a speech to text engine

---
+++ microsoft.js
@@ -0,0 +1,73 @@
+const { ConversationTranscriptionCanceledEventArgs } = require('microsoft-cognitiveservices-speech-sdk'),
+      { TranscriptionServiceRecognizer }             = require('microsoft-cognitiveservices-speech-sdk/distrib/lib/src/common.speech/Exports'),
+      linear16                                       = require('linear16'), // Encodes supplied file to adhere to "LINEAR16" encoding
+      path                                           = require("path"),
+      sdk                                            = require("microsoft-cognitiveservices-speech-sdk"),
+      fs                                             = require('fs');
+      
+
+// Create a speech config object
+const speechConfig = sdk.SpeechConfig.fromSubscription("6876b4deab4e437ca215c41fb02825f4", "canadacentral");
+
+let file = path.join(__dirname,'public','uploads','output.raw');
+
+async function main() { 
+    
+    // File name after encoding conversion
+    let res = "";
+
+    await linear16(path.join(__dirname,'public','uploads','blob.wav'), file)
+        .then(async () => {
+
+            res = await transcribe();
+
+        })
+        .catch((err) =>{
+            console.log(err);
+        });
+    
+
+    console.log(`RESULT: ${res}`);
+   
+
+    return res;
+
+}
+function transcribe() {
+/* 
+Returns a promise that sends a request to speech recognition API
+*/
+    return new Promise((resolve, reject) => {
+
+        let recognizer = create_recognizer(file);
+    
+        recognizer.recognizeOnceAsync(result => {
+            
+            recognizer.close();
+    
+            resolve(result.text);
+        });
+
+    })
+
+}
+
+function create_recognizer(filename){
+/* 
+* Creates a recognizer object to use to make api calls for speech recognition.
+*/  
+    let pushStream = sdk.AudioInputStream.createPushStream();
+    
+    fs.createReadStream(filename).on('data', function(arrayBuffer) {
+        pushStream.write(arrayBuffer.slice());
+    }).on('end', function() {
+        pushStream.close();
+    });
+    
+    let audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+
+    return new sdk.SpeechRecognizer(speechConfig, audioConfig);
+    
+}
+
+module.exports = main;
